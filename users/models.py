3dff267936164525942680f1d00bb064ab1aee4e
anastasia_lykova <anastasia.7@mail.ru>
2023-10-23 22:44:55 +0500

    users app

---
+++ users/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(max_length=100, verbose_name='почта', unique=True)
+    phone = models.CharField(max_length=35, verbose_name='номер телефона', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+    country = models.CharField(max_length=35, verbose_name='страна', **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
