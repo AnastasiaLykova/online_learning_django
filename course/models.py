4b4473c3485aeaa63bd83f47f40f2943b23fac8f
anastasia_lykova <anastasia.7@mail.ru>
2023-10-23 22:44:35 +0500

    course app

---
+++ course/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=250, verbose_name='название')
+    preview = models.ImageField(upload_to='course/', verbose_name='изображение', **NULLABLE)
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=250, verbose_name='название')
+    preview = models.ImageField(upload_to='course/', verbose_name='изображение', **NULLABLE)
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+    lesson_url = models.TextField(verbose_name='ссылка', **NULLABLE)
+    course = models.ForeignKey(Course, verbose_name='курс', on_delete=models.CASCADE, **NULLABLE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
