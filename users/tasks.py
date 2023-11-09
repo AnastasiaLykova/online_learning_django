23d116df15968e0e7eae871a18487890b40a4dd5
anastasia_lykova <anastasia.7@mail.ru>
2023-11-10 01:57:20 +0500

    fix_2

--- users/tasks.py
+++ users/tasks.py
@@ -9,3 +9,4 @@ def deactivate_user():
     for user in users:
         if user.last_login >= user.last_login+datetime.timedelta(days=30) and user.is_active:
             user.is_active = False
+            user.save()
